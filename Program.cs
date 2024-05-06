9993f2f33d2e4942fda646794932e5d36233e634
duvan <duvanmmontoya@gmail.com>
2024-05-05 20:34:13 -0500

    Permisos configuration

--- Program.cs
+++ Program.cs
@@ -6,15 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 //configuration
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowOrigin",
+    options.AddPolicy("AllowAnyOrigin",
         builder =>
         {
             builder.AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
         });
 });
-
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
